8416e64bb0e40159ced152b05089f41f600ce270
M Akif Eyler <ma.eyler@gmail.com>
2019-01-30 15:27:50 +0300

    make PWA

---
+++ hava/sw.js
@@ -0,0 +1,28 @@
+const CACHE ='Sun'
+function installCB(e) {
+  console.log('install', e.request);
+}
+self.addEventListener('install', installCB)
+
+function save(req, resp) {
+  if (!req.url.includes("github")) return resp;
+  return caches.open(CACHE)
+  .then(cache => {
+    cache.put(req, resp.clone());
+    return resp;
+  }) 
+  .catch(console.err)
+}
+function report(req) {
+  console.log(CACHE+' matches '+req.url)
+  return req
+}
+function fetchCB(e) { //fetch first
+  let req = e.request
+  e.respondWith(
+    fetch(req).then(r2 => save(req, r2))
+    .catch(() => caches.match(req).then(report))
+  )
+}
+self.addEventListener('fetch', fetchCB)
+
