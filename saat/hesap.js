62900b75166a38726c1246c4cf1b38880e6ce409
M Akif Eyler <ma.eyler@gmail.com>
2021-01-09 14:12:14 +0300

    improvements
    * takvim, harita, hesap

--- saat/hesap.js
+++ saat/hesap.js
@@ -1,26 +1,29 @@
 "use strict"
 const
-    VERSION = 'V0.6',
+    VERSION = 'V0.7',
     PI_180 = Math.PI/180, //for radian-degree conversion
     J2000 = 10958,  //number of days between 1970 and 2000 
-    HEADER = "Day        EqTime Delta Declin  Noon   Sunset"
+    HEADER = "Day        Delta EqTime Declin  Noon   Sunset"
 /** 
  * Immutable class for latitude, longitude, Time Zone
  **/
 class Location {
-    constructor(lat, lng, zone) {
-        this.lat = Number(lat) || 0
-        this.lng = Number(lng) || 0
+    constructor(lat=0, lng=0, zone) {
+        this.lat = Number(lat)
+        this.lng = Number(lng)
      // if (!zone) zone = -new Date().getTimezoneOffset()/60
-        this.zone = Number(zone) || Math.round(lng/15)
+        this.zone = (zone == undefined)?
+            Math.round(lng/15) : Number(zone)
     }
     toString() {
         let s = this.zone<0? '' : '+'
         return this.lat+', '+ this.lng+', '+s+this.zone
     }
     static fromString(s) {
+        let f = n => !n ? 0 :
+            n.length<7 ? n : Number(n).toFixed(4)
         let [lat, lng, zone] = s.split(/[ ,]+/)
-        return new Location(lat, lng, zone)
+        return new Location(f(lat), f(lng), zone)
     }
 }
 /** 
@@ -33,16 +36,13 @@ class Day {
         this.num = d  //integer number of days since 1/1/2000
         let date = new Date((d + J2000)*86400*1000)
         //this date shows the given day at midnight
-        this.str = date.getFullYear()+'-'
-            +F.d2(date.getMonth()+1)+'-'+F.d2(date.getDate())
+        this.str = F.stringFrom(date)
         let {eqTime, declin} = Day.calculate(d)
         this.eqTime = eqTime; this.declin = declin
         this.delta = (eqTime - Day.calculate(d+1).eqTime)*60
     }
     toString() {
-        return 'Day '+ this.num
-            +', eqTime='+this.eqTime.toFixed(2)+"'"
-            +', declin='+this.declin.toFixed(2)+'°'
+        return 'Day '+ this.num+': '+this.str
     }
     static fromString(s) {
         return new Day(new Date(s))
@@ -84,9 +84,12 @@ class SunData { //Singleton instance G is used
         this.half = this.timeOf(-1); //sunset-noon difference
     }
     toString() {
-        return this.day+'\nLoc ('+this.loc+')\n'+F.report()
+        let s = this.day
+        if (this.loc) s += '\nLoc '+F.summary()
+        return s
     }
 }
+const Kabe = new Location(21.422500, 39.826137, 2)
 /**
  * Utility class for Math -- static methods
  **/
@@ -98,27 +101,49 @@ class M {
     static arccos = x => Math.acos(x) / PI_180
     static arcsin = x => Math.asin(x) / PI_180
     static arctan2 = (x, y) => Math.atan2(x, y) / PI_180
+    static qiblah({lat, lng}) {
+        let d = lng - Kabe.lng
+        let y = M.sin(lat)*M.cos(d) - M.cos(lat)*M.tan(Kabe.lat)
+        return M.arctan2(M.sin(d), y).toFixed(2)
+    }
     static normal(x) {
         while (x >= 360) x -= 360;
         while (x < 0) x += 360;
         return x;  // 0<=x<360
     }
 }
+const d2 = n => (n<10? '0'+n : ''+n)
 /**
  * Utility class for format -- static methods
  **/
 class F {
+    static stringFrom(date) {
+        return date.getFullYear()
+        +'-'+d2(date.getMonth()+1)
+        +'-'+d2(date.getDate())
+    }
     static toHHMM(m) {
         m += 0.5 //trunc avoids 7:60
         let h = Math.trunc(m/60)
         let n = Math.trunc(m-60*h)
-        return F.d2(h)+':'+F.d2(n)
+        return d2(h)+':'+d2(n)
     }
-    static d2 = n => (n<10? '0'+n : ''+n)
+    static summary(str='') {
+        return str+'('+G.loc+') '
+            + F.toHHMM(G.noon-G.half).padStart(6)
+            + F.toHHMM(G.noon).padStart(6)
+            + F.toHHMM(G.noon+G.half).padStart(6)
+    }
+    static vakit() {
+        return '<b>Diyanet</b>'
+            +'\nGüneş '+ F.toHHMM(G.noon-G.half-6)
+            +'\nÖğle  '+ F.toHHMM(G.noon+6)
+            +'\nAkşam '+ F.toHHMM(G.noon+G.half+7)
+    }
     static report() {
         return G.day.str
-            + G.day.eqTime.toFixed(1).padStart(6)+"'"
             + G.day.delta.toFixed(0).padStart(5)+'"'
+            + G.day.eqTime.toFixed(1).padStart(6)+"'"
             + G.day.declin.toFixed(1).padStart(6)+'°'
             + F.toHHMM(G.noon).padStart(7)
             + F.toHHMM(G.noon+G.half).padStart(7)
