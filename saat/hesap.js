9a2eea8f587f2eddf46848f45e9711da8d6fea92
M Akif Eyler <ma.eyler@gmail.com>
2021-02-17 21:01:18 +0300

    emulate static methods
    for compatibility

--- saat/hesap.js
+++ saat/hesap.js
@@ -1,6 +1,6 @@
 "use strict"
 const
-    VERSION = 'V0.7',
+    VERSION = 'V0.8',
     PI_180 = Math.PI/180, //for radian-degree conversion
     J2000 = 10958,  //number of days between 1970 and 2000 
     HEADER = "Day        Delta EqTime Declin  Noon   Sunset"
@@ -19,13 +19,14 @@ class Location {
         let s = this.zone<0? '' : '+'
         return this.lat+', '+ this.lng+', '+s+this.zone
     }
-    static fromString(s) {
-        let f = n => !n ? 0 :
-            n.length<7 ? n : Number(n).toFixed(4)
-        let [lat, lng, zone] = s.split(/[ ,]+/)
-        return new Location(f(lat), f(lng), zone)
-    }
 }
+Location.fromString = (s) => { // emulate static method
+    let f = n => !n ? 0 :
+        n.length<7 ? n : Number(n).toFixed(4)
+    let [lat, lng, zone] = s.split(/[ ,]+/)
+    return new Location(f(lat), f(lng), zone)
+}
+
 /** 
  * Immutable class for equation of time and declination at d
  **/
@@ -44,20 +45,18 @@ class Day {
     toString() {
         return 'Day '+ this.num+': '+this.str
     }
-    static fromString(s) {
-        return new Day(new Date(s))
-    }
-    static calculate(d) {
-        let g = M.normal(357.529 + 0.98560028*d) //in degrees
-        let q = M.normal(280.459 + 0.98564736*d) //365.242 days
-        let L = M.normal(q + 1.915*M.sin(g) + 0.020*M.sin(2*g))
-        let e = 23.439 - 0.00000036* d //tiny correction
-        let RA = M.normal(M.arctan2(M.cos(e)*M.sin(L), M.cos(L)))
-        let eqTime = 4*(q - RA)  //equation of time in minutes
-        if (q>300 && RA<50) eqTime -= 4*360
-        let declin = M.arcsin(M.sin(e)* M.sin(L)) //declination
-        return {eqTime, declin}
-    }
+}
+Day.fromString = (s) => new Day(new Date(s))
+Day.calculate = (d) => {
+    let g = M.normal(357.529 + 0.98560028*d) //in degrees
+    let q = M.normal(280.459 + 0.98564736*d) //365.242 days
+    let L = M.normal(q + 1.915*M.sin(g) + 0.020*M.sin(2*g))
+    let e = 23.439 - 0.00000036* d //tiny correction
+    let RA = M.normal(M.arctan2(M.cos(e)*M.sin(L), M.cos(L)))
+    let eqTime = 4*(q - RA)  //equation of time in minutes
+    if (q>300 && RA<50) eqTime -= 4*360
+    let declin = M.arcsin(M.sin(e)* M.sin(L)) //declination
+    return {eqTime, declin}
 }
 /** 
  * Singleton for noon and sunset at given Location and Day
@@ -93,21 +92,21 @@ const Kabe = new Location(21.422500, 39.826137, 3)
 /**
  * Utility class for Math -- static methods
  **/
-class M {
+const M = {
     //all data is in degrees, converted to radians
-    static cos = x => Math.cos(x * PI_180)
-    static sin = x => Math.sin(x * PI_180)
-    static tan = x => Math.tan(x * PI_180)
-    static arccos = x => Math.acos(x) / PI_180
-    static arcsin = x => Math.asin(x) / PI_180
-    static arctan2 = (x, y) => Math.atan2(x, y) / PI_180
-    static qiblah(p) {
+    cos: x => Math.cos(x * PI_180),
+    sin: x => Math.sin(x * PI_180),
+    tan: x => Math.tan(x * PI_180),
+    arccos: x => Math.acos(x) / PI_180,
+    arcsin: x => Math.asin(x) / PI_180,
+    arctan2: (x, y) => Math.atan2(x, y) / PI_180,
+    qiblah: (p) => {
         let d = p.lng - Kabe.lng
         let x = M.sin(p.lat)*M.cos(d)
         let y = M.cos(p.lat)*M.tan(Kabe.lat)
         return M.arctan2(M.sin(d), x-y).toFixed(2)
-    }
-    static normal(x) {
+    },
+    normal: (x) => {
         while (x >= 360) x -= 360;
         while (x < 0) x += 360;
         return x;  // 0<=x<360
@@ -117,37 +116,29 @@ const d2 = n => (n<10? '0'+n : ''+n)
 /**
  * Utility class for format -- static methods
  **/
-class F {
-    static stringFrom(date) {
-        return date.getFullYear()
+const F = {
+    stringFrom: (date) => date.getFullYear()
         +'-'+d2(date.getMonth()+1)
-        +'-'+d2(date.getDate())
-    }
-    static toHHMM(m) {
+        +'-'+d2(date.getDate()),
+    toHHMM: (m) => {
         m += 0.5 //trunc avoids 7:60
         let h = Math.trunc(m/60)
         let n = Math.trunc(m-60*h)
         return d2(h)+':'+d2(n)
-    }
-    static summary(str='') {
-        return str+'('+G.loc+') '
+    },
+    summary: (str='') => str+'('+G.loc+') '
             + F.toHHMM(G.noon-G.half).padStart(6)
             + F.toHHMM(G.noon).padStart(6)
-            + F.toHHMM(G.noon+G.half).padStart(6)
-    }
-    static vakit() {
-        return '<b>Diyanet</b>'
+            + F.toHHMM(G.noon+G.half).padStart(6),
+    vakit: () => '<b>Diyanet</b>'
             +'\nGüneş '+ F.toHHMM(G.noon-G.half-6)
             +'\nÖğle  '+ F.toHHMM(G.noon+6)
-            +'\nAkşam '+ F.toHHMM(G.noon+G.half+7)
-    }
-    static report() {
-        return G.day.str
+            +'\nAkşam '+ F.toHHMM(G.noon+G.half+7),
+    report: () => G.day.str
             + G.day.delta.toFixed(0).padStart(5)+'"'
             + G.day.eqTime.toFixed(1).padStart(6)+"'"
             + G.day.declin.toFixed(1).padStart(6)+'°'
             + F.toHHMM(G.noon).padStart(7)
             + F.toHHMM(G.noon+G.half).padStart(7)
-    }
 }
 const G = new SunData()  //global object for current data
